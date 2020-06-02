1a066459e421f5990a9b36f1730199cb6a80feb2
Minh Tri <handcock.dk@gmail.com>
2020-06-02 15:20:32 +0700

    again

--- src/components/Home/HomeBody/HomeBody.js
+++ src/components/Home/HomeBody/HomeBody.js
@@ -15,7 +15,7 @@ class HomeBody extends React.Component {
                         <p>Create your quiz and share to everyone </p>
                     </div>
                     <button className="home-body-btn">
-                        Get beginning
+                        Get beginning again
                     </button>
                 </div>
             </div>
